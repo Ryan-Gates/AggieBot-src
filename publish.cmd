650c0897568847923c2e3e3d46ad691cef899191
Ryan Gates <rpgates@suddenlink.net>
2017-12-14 22:50:17 -0800

    Init Repo

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=aggiebot-Web-Deploy.pubxml -p:Password=G0bqk5kEwcwmx8CDGJLy43d46aKNbcvJcY1bCNroCSmxLxd6aMQwkvHNGffi
+
